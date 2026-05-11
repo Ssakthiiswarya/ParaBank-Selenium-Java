18b25891a32d5d367abb97e03e48881e1b2d893f
ishu <ishu@gmail.com>
2026-05-11 15:12:13 +0530

    Completed fund transfer module

---
+++ src/test/java/pages/TransferPage.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TransferPage {
+
+    WebDriver driver;
+
+    public TransferPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By transferFundsLink = By.linkText("Transfer Funds");
+
+    By amountField = By.id("amount");
+
+    By transferButton = By.xpath("//input[@value='Transfer']");
+
+    By successMessage = By.className("title");
+
+    public void clickTransferFunds() {
+
+        driver.findElement(transferFundsLink).click();
+    }
+
+    public void enterAmount(String amount) {
+
+        driver.findElement(amountField).clear();
+
+        driver.findElement(amountField).sendKeys(amount);
+    }
+
+    public void clickTransferButton() {
+
+        driver.findElement(transferButton).click();
+    }
+
+    public String getSuccessMessage() {
+
+        return driver.findElement(successMessage).getText();
+    }
+
+    public String getPageSource() {
+
+        return driver.getPageSource();
+    }
+}
---
+++ src/test/java/tests/TransferTest.java
@@ -0,0 +1,90 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.TransferPage;
+
+public class TransferTest extends BaseTest {
+
+    @Test
+    public void verifyFundTransferTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransferPage transferPage = new TransferPage(driver);
+
+        transferPage.clickTransferFunds();
+
+        transferPage.enterAmount("100");
+
+        transferPage.clickTransferButton();
+
+        String message = transferPage.getSuccessMessage();
+
+        Assert.assertNotNull(message);
+    }
+
+    @Test
+    public void verifyTransferredAmountTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransferPage transferPage = new TransferPage(driver);
+
+        transferPage.clickTransferFunds();
+
+        transferPage.enterAmount("200");
+
+        transferPage.clickTransferButton();
+
+        String page = transferPage.getPageSource();
+
+        Assert.assertNotNull(page);
+    }
+
+    @Test
+    public void verifyInvalidAmountTransferTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransferPage transferPage = new TransferPage(driver);
+
+        transferPage.clickTransferFunds();
+
+        transferPage.enterAmount("-100");
+
+        transferPage.clickTransferButton();
+
+        String page = transferPage.getPageSource();
+
+        Assert.assertNotNull(page);
+    }
+
+    @Test
+    public void verifyEmptyAmountTransferTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransferPage transferPage = new TransferPage(driver);
+
+        transferPage.clickTransferFunds();
+
+        transferPage.enterAmount("");
+
+        transferPage.clickTransferButton();
+
+        String currentUrl = driver.getCurrentUrl();
+
+        Assert.assertNotNull(currentUrl);
+    }
+}
