4fbc7c391825dfa0706347996f915c3d7fd07c6a
ishu <ishu@gmail.com>
2026-05-11 16:05:14 +0530

    Completed transaction search module

---
+++ src/test/java/pages/TransactionPage.java
@@ -0,0 +1,51 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TransactionPage {
+
+    WebDriver driver;
+
+    public TransactionPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By findTransactionsLink = By.linkText("Find Transactions");
+
+    By amountField = By.id("criteria.amount");
+
+    By findByAmountButton = By.id("findByAmount");
+
+    By fromDateField = By.id("criteria.fromDate");
+
+    By toDateField = By.id("criteria.toDate");
+
+    By findByDateButton = By.id("findByDateRange");
+
+    public void clickFindTransactions() {
+
+        driver.findElement(findTransactionsLink).click();
+    }
+
+    public void searchByAmount(String amount) {
+
+        driver.findElement(amountField).sendKeys(amount);
+
+        driver.findElement(findByAmountButton).click();
+    }
+
+    public void searchByDate(String fromDate, String toDate) {
+
+        driver.findElement(fromDateField).sendKeys(fromDate);
+
+        driver.findElement(toDateField).sendKeys(toDate);
+
+        driver.findElement(findByDateButton).click();
+    }
+
+    public String getPageSource() {
+
+        return driver.getPageSource();
+    }
+}
---
+++ src/test/java/tests/TransactionTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.TransactionPage;
+
+public class TransactionTest extends BaseTest {
+
+    @Test
+    public void verifyTransactionSearchByAmountTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransactionPage transactionPage = new TransactionPage(driver);
+
+        transactionPage.clickFindTransactions();
+
+        String currentUrl = driver.getCurrentUrl();
+
+        Assert.assertNotNull(currentUrl);
+    }
+
+    @Test
+    public void verifyTransactionSearchByDateTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransactionPage transactionPage = new TransactionPage(driver);
+
+        transactionPage.clickFindTransactions();
+
+        String title = driver.getTitle();
+
+        Assert.assertNotNull(title);
+    }
+
+    @Test
+    public void verifyEmptyTransactionSearchTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginToApplication("john", "demo");
+
+        TransactionPage transactionPage = new TransactionPage(driver);
+
+        transactionPage.clickFindTransactions();
+
+        String page = driver.getPageSource();
+
+        Assert.assertNotNull(page);
+    }
+}
